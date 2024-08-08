97153258c9a868f57f6ee97558e7452e258320ba
Haru-93 <hal-ishikawa2046@outlook.jp>
2024-08-08 17:40:31 +0900

    Nested Routes and Outlets

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,18 @@
+import type { LinksFunction } from "@remix-run/node";
+
 import {
   Form,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import appStyleHref from "./app.css?url"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStyleHref},
+];
 
 export default function App() {
   return (
@@ -43,6 +51,9 @@ export default function App() {
               </li>
             </ul>
           </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
